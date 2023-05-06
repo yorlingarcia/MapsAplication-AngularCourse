ea4f855cab687084e2ba7df224dc8a7cfcb8716d
Yorlin Garcia <yorlingarcia96@hotmail.com>
2023-05-06 14:00:22 -0500

    creacion de lista para acceso a cada ruta

--- src/app/maps/components/side-menu/side-menu.component.ts
+++ src/app/maps/components/side-menu/side-menu.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
 
+interface MenuItem {
+  router: string;
+  name: string;
+}
+
 @Component({
   selector: 'app-side-menu',
   templateUrl: './side-menu.component.html',
-  styleUrls: ['./side-menu.component.css']
+  styleUrls: ['./side-menu.component.css'],
 })
 export class SideMenuComponent {
-
+  public menuItems: MenuItem[] = [
+    { router: '/maps/fullscreen', name: 'Full-Screen' },
+    { router: '/maps/zoom-range', name: 'Zoom-Range' },
+    { router: '/maps/markers', name: 'Markers' },
+    { router: '/maps/properties', name: 'Houses' },
+  ];
 }
