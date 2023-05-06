a396adbf5a5c41dbd893b05df72b79327ebc8e64
Yorlin Garcia <yorlingarcia96@hotmail.com>
2023-05-06 14:59:03 -0500

    uso del ngAfterViewInit para el uso del mapa, para esperar que el html se renderice

--- src/app/maps/pages/full-screen-page/full-screen-page.component.ts
+++ src/app/maps/pages/full-screen-page/full-screen-page.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
 
 import * as mapboxgl from 'mapbox-gl'; // or "const mapboxgl = require('mapbox-gl');"
 
@@ -9,4 +9,13 @@ import * as mapboxgl from 'mapbox-gl'; // or "const mapboxgl = require('mapbox-g
   templateUrl: './full-screen-page.component.html',
   styleUrls: ['./full-screen-page.component.css'],
 })
-export class FullScreenPageComponent {}
+export class FullScreenPageComponent implements AfterViewInit {
+  ngAfterViewInit(): void {
+    const map = new mapboxgl.Map({
+      container: 'map', // container ID
+      style: 'mapbox://styles/mapbox/streets-v12', // style URL
+      center: [-74.5, 40], // starting position [lng, lat]
+      zoom: 9, // starting zoom
+    });
+  }
+}
