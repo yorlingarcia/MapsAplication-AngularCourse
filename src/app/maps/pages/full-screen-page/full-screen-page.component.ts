0cc06824e1a2801c874ce7aa715b07fcf67abde9
Yorlin Garcia <yorlingarcia96@hotmail.com>
2023-05-06 14:57:08 -0500

    aceso al token, se dio tipo de any

--- src/app/maps/pages/full-screen-page/full-screen-page.component.ts
+++ src/app/maps/pages/full-screen-page/full-screen-page.component.ts
@@ -2,6 +2,9 @@ import { Component } from '@angular/core';
 
 import * as mapboxgl from 'mapbox-gl'; // or "const mapboxgl = require('mapbox-gl');"
 
+(mapboxgl as any).accessToken =
+  'pk.eyJ1IjoieW9ybGluOTYiLCJhIjoiY2xoNWdzeTZiMWpycDNjbzF5eGIyNnZqaCJ9.9ZDRHEcNHabmbiCL7adZxw';
+
 @Component({
   templateUrl: './full-screen-page.component.html',
   styleUrls: ['./full-screen-page.component.css'],
