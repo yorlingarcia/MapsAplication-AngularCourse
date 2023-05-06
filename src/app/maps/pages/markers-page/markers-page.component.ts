bf46fa60c2f91dfe2695eb31c1799efe6f5ce3b1
Yorlin Garcia <yorlingarcia96@hotmail.com>
2023-05-06 13:45:37 -0500

    creacion de estructura

---
+++ src/app/maps/pages/markers-page/markers-page.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+  templateUrl: './markers-page.component.html',
+  styleUrls: ['./markers-page.component.css'],
+})
+export class MarkersPageComponent {}
---
+++ src/app/maps/pages/zoom-range-page/zoom-range-page.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+  templateUrl: './zoom-range-page.component.html',
+  styleUrls: ['./zoom-range-page.component.css'],
+})
+export class ZoomRangePageComponent {}
