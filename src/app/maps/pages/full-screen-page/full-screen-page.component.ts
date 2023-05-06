a21ada5ab2d5e07f71542995808bc3b237af2493
Yorlin Garcia <yorlingarcia96@hotmail.com>
2023-05-06 17:13:42 -0500

    implementacion de marcadores, de forma informal

--- src/app/maps/pages/full-screen-page/full-screen-page.component.ts
+++ src/app/maps/pages/full-screen-page/full-screen-page.component.ts
@@ -9,7 +9,7 @@ export class FullScreenPageComponent implements AfterViewInit {
   @ViewChild('map') divMap?: ElementRef;
 
   ngAfterViewInit(): void {
-    if (!this.divMap) throw 'El elemento html no fue encontrado';
+    if (!this.divMap) throw 'Ele elementos html no fue encontrado';
     const map = new Map({
       container: this.divMap?.nativeElement, // container ID
       style: 'mapbox://styles/mapbox/streets-v12', // style URL
--- src/app/maps/pages/markers-page/markers-page.component.ts
+++ src/app/maps/pages/markers-page/markers-page.component.ts
@@ -1,7 +1,36 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
+import { LngLat, Map, Marker } from 'mapbox-gl'; // or "const mapboxgl = require('mapbox-gl');"
 
 @Component({
   templateUrl: './markers-page.component.html',
   styleUrls: ['./markers-page.component.css'],
 })
-export class MarkersPageComponent {}
+export class MarkersPageComponent implements AfterViewInit {
+  @ViewChild('map') divMap?: ElementRef;
+
+  public map?: Map;
+  public currentCenter: LngLat = new LngLat(
+    -72.64665130464374,
+    7.3753924772996555
+  );
+
+  ngAfterViewInit(): void {
+    if (!this.divMap) throw 'El elemento html no fue encontrado';
+    this.map = new Map({
+      container: this.divMap?.nativeElement, // container ID
+      style: 'mapbox://styles/mapbox/streets-v12', // style URL
+      center: this.currentCenter, // starting position [lng, lat]
+      zoom: 10, // starting zoom
+    });
+
+    const markerHtml = document.createElement('div');
+    markerHtml.innerHTML = 'Yorlin Garcia';
+
+    const marker = new Marker({
+      //color: 'red'
+      element: markerHtml,
+    })
+      .setLngLat(this.currentCenter)
+      .addTo(this.map);
+  }
+}
