042a6b3a4efc828162d0478a7fa8c60dc9af78b4
Yorlin Garcia <yorlingarcia96@hotmail.com>
2023-05-06 16:39:49 -0500

    implementacion de la lectura de la longitud y latitud, ademas de la limpieza de los listeners

--- src/app/maps/pages/zoom-range-page/zoom-range-page.component.ts
+++ src/app/maps/pages/zoom-range-page/zoom-range-page.component.ts
@@ -1,24 +1,38 @@
-import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
-import { Map } from 'mapbox-gl'; // or "const mapboxgl = require('mapbox-gl');"
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  OnDestroy,
+  ViewChild,
+} from '@angular/core';
+import { LngLat, Map } from 'mapbox-gl'; // or "const mapboxgl = require('mapbox-gl');"
 
 @Component({
   templateUrl: './zoom-range-page.component.html',
   styleUrls: ['./zoom-range-page.component.css'],
 })
-export class ZoomRangePageComponent implements AfterViewInit {
+export class ZoomRangePageComponent implements AfterViewInit, OnDestroy {
   @ViewChild('map') divMap?: ElementRef;
   public currentZoom: number = 10;
   public map?: Map;
+  public currentCenter: LngLat = new LngLat(
+    -72.64665130464374,
+    7.3753924772996555
+  );
 
   ngAfterViewInit(): void {
     if (!this.divMap) throw 'El elemento html no fue encontrado';
     this.map = new Map({
       container: this.divMap?.nativeElement, // container ID
       style: 'mapbox://styles/mapbox/streets-v12', // style URL
-      center: [-74.5, 40], // starting position [lng, lat]
+      center: this.currentCenter, // starting position [lng, lat]
       zoom: this.currentZoom, // starting zoom
     });
     this.mapListeners();
+  }
+
+  ngOnDestroy(): void {
+    this.map?.remove();
   }
 
   mapListeners() {
@@ -30,6 +44,10 @@ export class ZoomRangePageComponent implements AfterViewInit {
     this.map.on('zoomend', (ev) => {
       if (this.map!.getZoom() < 18) return;
       this.map?.zoomTo(18);
+    });
+
+    this.map.on('move', () => {
+      this.currentCenter = this.map!.getCenter();
     });
   }
 
