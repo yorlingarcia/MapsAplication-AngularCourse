3f8952c9c47d2b86d55215e7dddf178ba6d6aeda
Yorlin Garcia <yorlingarcia96@hotmail.com>
2023-05-06 22:50:55 -0500

    cartas de descripcion de algunos lugares, implementacion con reutilizacion del componente

--- src/app/maps/components/mini-map/mini-map.component.ts
+++ src/app/maps/components/mini-map/mini-map.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  Input,
+  ViewChild,
+} from '@angular/core';
+
+import { Map, Marker } from 'mapbox-gl'; // or "const mapboxgl = require('mapbox-gl');"
 
 @Component({
   selector: 'app-mini-map',
   templateUrl: './mini-map.component.html',
-  styleUrls: ['./mini-map.component.css']
+  styleUrls: ['./mini-map.component.css'],
 })
-export class MiniMapComponent {
+export class MiniMapComponent implements AfterViewInit {
+  @Input() lngLat?: [number, number];
+  @ViewChild('map') divMap?: ElementRef;
 
+  ngAfterViewInit() {
+    if (!this.divMap?.nativeElement) throw 'El tag divMap no fue encontrado';
+    if (!this.lngLat) throw 'LngLat no puede ser nulo';
+
+    //mapa
+    const map = new Map({
+      container: this.divMap?.nativeElement, // container ID
+      style: 'mapbox://styles/mapbox/streets-v12', // style URL
+      center: this.lngLat, // starting position [lng, lat]
+      zoom: 14, // starting zoom
+      interactive: false,
+    });
+    //marker
+    new Marker().setLngLat(this.lngLat).addTo(map);
+  }
 }
